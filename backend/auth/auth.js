5acc4ff400b6d854f6506548468b8e120b2e07a8
AnkuThakur704 <ankushanku704@gmail.com>
2026-09-11 22:13:32 +0530

    cookie attributes

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -4,24 +4,30 @@ import login from './login.js'
 
 const authrouter = express.Router()
 
-authrouter.post("/signup",async (req,res)=>{
-    const flag =  await signup(req.body.name, req.body.email, req.body.password)
-    res.json({success:flag})
+authrouter.post("/signup", async (req, res) => {
+    const flag = await signup(req.body.name, req.body.email, req.body.password)
+    res.json({ success: flag })
 })
 
-authrouter.post("/login",async (req,res)=>{
+authrouter.post("/login", async (req, res) => {
     const data = await login(req)
     console.log("status: ", data)
-    if(data.status){
+    if (data.status) {
         console.log("cookie sent")
-        res.cookie("sessionToken", data.token,{maxAge: 1000*60*60*24})
+        res.cookie("sessionToken", data.token, {
+            maxAge: 1000 * 60 * 60 * 24,
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        }
+        )
     }
-    res.json({success:data.status})
+    res.json({ success: data.status })
 })
 
-authrouter.post("/logout", (req,res)=>{
+authrouter.post("/logout", (req, res) => {
     res.clearCookie("sessionToken")
-    res.status(200).json({message:"logged out successfully"})
+    res.status(200).json({ message: "logged out successfully" })
 })
 
 export default authrouter
